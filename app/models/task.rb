8bd610e95ce1b8e9828a13e2862a9464b2e1b366
Samuel Espinet <samuelespinet30@gmail.com>
2022-11-14 13:34:07 +1000

    add restaurant model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,11 @@
 class Task < ApplicationRecord
+  def list
+    create_table :task do |t|
+      t.string :title
+      t.string :details
+      t.string :completed false
+
+      t.timestamps
+    end
+  end
 end
